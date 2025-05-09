2199a0a3b022dfeb5f641ce1e4e94316091e5617
shams03 <95904933+shams03@users.noreply.github.com>
2025-05-09 11:53:57 +0530

    fixed prisma schema

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -12,6 +12,17 @@ export const projectRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       // ctx.user.userId
+      const project = await ctx.db.project.create({
+        data:{
+          name:input.name,
+          githubUrl:input.githubUrl,
+          userToProjects:{
+            create:{
+              userId:ctx.user.userId!,
+            }
+          }
+        }
+      })
       console.log("input", input);
       return true;
     }),
